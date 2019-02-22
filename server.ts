e3f382fb1bb2fd14108891b7f8ead2172a910dc8
hyeock gu <mb7878@naver.com>
2019-02-22 19:20:08 +0900

    :)

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 const Port = 3000;
 
-const server = app.listen(Port , '0.0.0.0' , err => {
-    console.log(`connection server ${Port}`)
-})
+const server = app.listen(Port , "0.0.0.0" , (err: any) => {
+    if (err) {
+        throw(err);
+    }
+    console.log(`connection server ${Port}`);
+});
